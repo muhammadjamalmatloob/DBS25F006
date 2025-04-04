d42682de58593912c7d1295be7db340c96a09fab
muhammadjamalmatloob <jamalmatloob786@gmail.com>
2025-04-04 12:16:12 +0500

    login added

--- DBFinalProject/DBFinalProject/Form1.Designer.cs
+++ DBFinalProject/DBFinalProject/Form1.Designer.cs
@@ -31,11 +31,26 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainInterface));
             this.kryptonPalette1 = new ComponentFactory.Krypton.Toolkit.KryptonPalette(this.components);
-            this.panel1 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.title = new System.Windows.Forms.Label();
-            this.panel1.SuspendLayout();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.login = new System.Windows.Forms.Label();
+            this.Closebtn = new System.Windows.Forms.Button();
+            this.password = new System.Windows.Forms.TextBox();
+            this.password_panel = new System.Windows.Forms.Panel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.user_panel = new System.Windows.Forms.Panel();
+            this.user = new System.Windows.Forms.PictureBox();
+            this.username = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.forgot = new System.Windows.Forms.LinkLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.password_panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.user_panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.user)).BeginInit();
             this.SuspendLayout();
             // 
             // kryptonPalette1
@@ -76,65 +91,219 @@
             this.kryptonPalette1.HeaderStyles.HeaderForm.StateCommon.ButtonEdgeInset = 10;
             this.kryptonPalette1.HeaderStyles.HeaderForm.StateCommon.Content.Padding = new System.Windows.Forms.Padding(10, -1, -1, -1);
             // 
-            // panel1
-            // 
-            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel1.BackColor = System.Drawing.Color.YellowGreen;
-            this.panel1.Controls.Add(this.title);
-            this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Location = new System.Drawing.Point(-7, 3);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1000, 125);
-            this.panel1.TabIndex = 0;
-            // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(3, 0);
+            this.pictureBox1.Location = new System.Drawing.Point(-3, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(133, 125);
+            this.pictureBox1.Size = new System.Drawing.Size(287, 501);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // title
+            // pictureBox2
             // 
-            this.title.AutoSize = true;
-            this.title.BackColor = System.Drawing.Color.YellowGreen;
-            this.title.Font = new System.Drawing.Font("Palatino Linotype", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.title.ForeColor = System.Drawing.Color.MidnightBlue;
-            this.title.Location = new System.Drawing.Point(183, 41);
-            this.title.Name = "title";
-            this.title.Size = new System.Drawing.Size(303, 44);
-            this.title.TabIndex = 1;
-            this.title.Text = "Apex Bank Limited";
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(442, 48);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(102, 92);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // login
+            // 
+            this.login.AutoSize = true;
+            this.login.Font = new System.Drawing.Font("Book Antiqua", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.login.Location = new System.Drawing.Point(373, 155);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(241, 26);
+            this.login.TabIndex = 2;
+            this.login.Text = "Login To Your Account ";
+            this.login.Click += new System.EventHandler(this.login_Click);
+            // 
+            // Closebtn
+            // 
+            this.Closebtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Closebtn.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Closebtn.FlatAppearance.BorderSize = 0;
+            this.Closebtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Closebtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Closebtn.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.Closebtn.Location = new System.Drawing.Point(667, 0);
+            this.Closebtn.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            this.Closebtn.Name = "Closebtn";
+            this.Closebtn.Size = new System.Drawing.Size(34, 35);
+            this.Closebtn.TabIndex = 3;
+            this.Closebtn.Text = "X";
+            this.Closebtn.UseVisualStyleBackColor = true;
+            this.Closebtn.Click += new System.EventHandler(this.Closebtn_Click);
+            // 
+            // password
+            // 
+            this.password.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.password.Font = new System.Drawing.Font("Book Antiqua", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.password.Location = new System.Drawing.Point(53, 9);
+            this.password.Name = "password";
+            this.password.PasswordChar = '•';
+            this.password.Size = new System.Drawing.Size(196, 20);
+            this.password.TabIndex = 4;
+            // 
+            // password_panel
+            // 
+            this.password_panel.BackColor = System.Drawing.Color.White;
+            this.password_panel.Controls.Add(this.pictureBox3);
+            this.password_panel.Controls.Add(this.password);
+            this.password_panel.Location = new System.Drawing.Point(360, 268);
+            this.password_panel.Name = "password_panel";
+            this.password_panel.Size = new System.Drawing.Size(254, 37);
+            this.password_panel.TabIndex = 5;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(7, 5);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(31, 27);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // user_panel
+            // 
+            this.user_panel.BackColor = System.Drawing.Color.White;
+            this.user_panel.Controls.Add(this.user);
+            this.user_panel.Controls.Add(this.username);
+            this.user_panel.Location = new System.Drawing.Point(360, 207);
+            this.user_panel.Name = "user_panel";
+            this.user_panel.Size = new System.Drawing.Size(254, 37);
+            this.user_panel.TabIndex = 6;
+            // 
+            // user
+            // 
+            this.user.Image = ((System.Drawing.Image)(resources.GetObject("user.Image")));
+            this.user.Location = new System.Drawing.Point(7, 5);
+            this.user.Name = "user";
+            this.user.Size = new System.Drawing.Size(31, 27);
+            this.user.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.user.TabIndex = 5;
+            this.user.TabStop = false;
+            // 
+            // username
+            // 
+            this.username.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.username.Font = new System.Drawing.Font("Book Antiqua", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.username.Location = new System.Drawing.Point(53, 9);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(196, 20);
+            this.username.TabIndex = 4;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(360, 322);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(102, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Show Password";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // forgot
+            // 
+            this.forgot.ActiveLinkColor = System.Drawing.Color.MediumSlateBlue;
+            this.forgot.AutoSize = true;
+            this.forgot.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.forgot.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.forgot.LinkColor = System.Drawing.Color.Blue;
+            this.forgot.Location = new System.Drawing.Point(522, 322);
+            this.forgot.Name = "forgot";
+            this.forgot.Size = new System.Drawing.Size(92, 13);
+            this.forgot.TabIndex = 8;
+            this.forgot.TabStop = true;
+            this.forgot.Text = "Forgot Password?";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.button1.Location = new System.Drawing.Point(360, 367);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 31);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.ActiveLinkColor = System.Drawing.Color.MediumSlateBlue;
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.LinkColor = System.Drawing.Color.Blue;
+            this.linkLabel1.Location = new System.Drawing.Point(492, 376);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(122, 13);
+            this.linkLabel1.TabIndex = 10;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Don\'t have an account?";
             // 
             // MainInterface
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
-            this.ClientSize = new System.Drawing.Size(984, 661);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(700, 450);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.forgot);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.user_panel);
+            this.Controls.Add(this.password_panel);
+            this.Controls.Add(this.Closebtn);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "MainInterface";
             this.Palette = this.kryptonPalette1;
             this.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.Custom;
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.password_panel.ResumeLayout(false);
+            this.password_panel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.user_panel.ResumeLayout(false);
+            this.user_panel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.user)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private ComponentFactory.Krypton.Toolkit.KryptonPalette kryptonPalette1;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label title;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label login;
+        private System.Windows.Forms.Button Closebtn;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Panel password_panel;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Panel user_panel;
+        private System.Windows.Forms.PictureBox user;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.LinkLabel forgot;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
     }
 }
 
--- DBFinalProject/DBFinalProject/Form1.cs
+++ DBFinalProject/DBFinalProject/Form1.cs
@@ -17,5 +17,27 @@ namespace DBFinalProject
         {
             InitializeComponent();
         }
+
+        private void login_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Closebtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                password.PasswordChar = '\0';
+            }
+            else
+            {
+                password.PasswordChar = '•';
+            }
+        }
     }
 }
