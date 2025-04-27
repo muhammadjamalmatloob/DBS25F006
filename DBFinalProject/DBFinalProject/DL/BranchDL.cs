777e5fb3dc9106d85be93fdbccca23028b433ca8
mumer2650 <2024cs191@student.uet.edu.pk>
2025-04-27 20:46:09 +0500

    added some backend

---
+++ DBFinalProject/DBFinalProject/DL/BranchDL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DBFinalProject.BL;
+using DBFinalProject.Utility;
+
+namespace DBFinalProject.DL
+{
+    internal class BranchDL
+    {
+        public static List<BranchBL> branchList = new List<BranchBL>();
+
+        public static void AddBranch(BranchBL branch)
+        {
+            branchList.Add(branch);
+        }
+
+        public static bool AddBranchInDb(BranchBL branch)
+        {
+            string query = $"INSERT INTO branches (branch_name, branch_code, address, contact, city, country) VALUES ('{branch.get_branch_name()}', {branch.get_branch_code()}, '{branch.get_address()}', '{branch.get_contact()}', '{branch.get_city()}', '{branch.get_country()}')";
+            return DatabaseHelper.Instance.Update(query) > 0;
+        }
+    }
+}
