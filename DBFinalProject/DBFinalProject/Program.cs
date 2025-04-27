a8fa1e62a6849c4648e7f652b00f5237f253abdb
Rumman2024CS214 <rummanahmedra237@gmail.com>
2025-04-27 22:14:56 +0500

    trash dont merge

--- DBFinalProject/DBFinalProject/Program.cs
+++ DBFinalProject/DBFinalProject/Program.cs
@@ -24,7 +24,7 @@ namespace DBFinalProject
             //MainInterface mainForm = new MainInterface();
             //kryptonManager.GlobalPalette = mainForm.myPallet;
 
-            Application.Run(new ViewBranchInfo());
+            Application.Run(new GeneralMenu());
         }
     }
 }
