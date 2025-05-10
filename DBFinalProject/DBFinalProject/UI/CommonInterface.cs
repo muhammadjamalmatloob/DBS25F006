480dd954332b52b0e2cfe63760094951f279dad2
Rumman2024CS214 <rummanahmedra237@gmail.com>
2025-05-10 16:20:34 +0500

    India ko heavy jawab dedia bhai

--- DBFinalProject/DBFinalProject/UI/CommonInterface.cs
+++ DBFinalProject/DBFinalProject/UI/CommonInterface.cs
@@ -21,17 +21,71 @@ namespace DBFinalProject
         private void kryptonButton1_Click(object sender, EventArgs e)
         {
             string username="";
-            string account_num = "";
+            string total_acc = "";
             decimal balance = 0;
             username = MainInterface.UserName;
+            int user_id = DL.UserDL.get_user_id(username);
+            int client_id = DL.ClientDL.getClientIdbyUserId(user_id);
+            total_acc = DL.AccountDL.TotalAccountForSpecificClient(client_id);
             label1.Text = username;
-            label6.Text = account_num;
-            balance = DL.AccountDL.getBalanceByNumber(account_num);
-            label2.Text = balance.ToString();
+            label6.Text = total_acc;
             Thread.Sleep(2000);
             label1.Text = "*******";
             label6.Text = "*******";
-            label2.Text = "*******";
+        }
+
+        private void kryptonComboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kryptonTextBox1_Enter(object sender, EventArgs e)
+        {
+            if(kryptonTextBox1.Text == "Enter Account Number")
+            {
+                kryptonTextBox1.Text = "";
+            }
+        }
+
+        private void kryptonTextBox1_Leave(object sender, EventArgs e)
+        {
+            if (kryptonTextBox1.Text == "")
+            {
+                kryptonTextBox1.Text = "Enter Account Number";
+            }
+        }
+
+        private void CommonInterface_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kryptonButton12_Click(object sender, EventArgs e)
+        {
+            GrpDep.Visible = false;
+        }
+
+        private void kryptonButton13_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string acc_num = kryptonTextBox1.Text;
+                decimal balance = DL.AccountDL.getBalanceByNumber(acc_num);
+                kryptonTextBox2.Text += balance.ToString();
+                Thread.Sleep(2000);
+                kryptonTextBox1.Text = "Enter Account Number";
+                kryptonTextBox2.Text = "balance: ";
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message,"Error");
+            }
+        }
+
+        private void kryptonButton2_Click(object sender, EventArgs e)
+        {
+            GrpDep.Visible = true;
         }
     }
 }
